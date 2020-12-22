452cb8e6d90cc72b18bc4ebdb93db5fb703b5c99
Gautami2612 <72113628+Gautami2612@users.noreply.github.com>
2020-12-22 18:16:38 -0500

    HW 13-1

---
+++ HW13-1/HW13_Multicast/src/edu/umb/cs680/hw013/DJIAQuoteObserver.java
@@ -0,0 +1,10 @@
+package edu.umb.cs680.hw013;
+
+import java.util.EventListener;
+
+	public interface DJIAQuoteObserver extends EventListener{
+	
+		
+		public void updateDJIA(DJIAEvent bondEvent);
+	
+	}
